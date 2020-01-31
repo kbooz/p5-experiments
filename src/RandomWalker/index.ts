43a626605553e25a03a993866ec5abe788ebeebe
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2020-01-30 22:52:11 -0300

    organized

---
+++ src/RandomWalker/index.ts
@@ -0,0 +1,16 @@
+import P5 from "p5";
+import RandomBall from "./RandomBall";
+
+export default function RandomWalker (p: P5) {
+    let walker: RandomBall;
+
+    p.setup = function() {
+        p.createCanvas(700, 410);
+        walker = new RandomBall(p);
+    };
+
+    p.draw = function() {
+        p.background(51);
+        walker.draw();
+    };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
 import P5 from "p5";
-import RandomBall from "./RandomBall";
+import RandomWalker from "./RandomWalker";
 
-new P5(function(p: P5) {
-  let walker: RandomBall;
-
-  p.setup = function() {
-    p.createCanvas(700, 410);
-    walker = new RandomBall(p);
-  };
-
-  p.draw = function() {
-    p.background(51);
-    walker.draw();
-  };
-});
+new P5(RandomWalker);
