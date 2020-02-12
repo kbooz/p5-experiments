bea8e843ab6caf64116b84d9d7be061651e925d9
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2020-02-11 23:36:28 -0300

    Added experiment selector

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,8 @@ module.exports = {
     rules: {
         "no-param-reassign" : [0],
         "no-new" : [0],
+        "react/static-property-placement": [0],
+        "no-plusplus": [0],
+        "prefer-destructuring": [0]
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
 import P5 from "p5";
-// import RandomWalkerMouse from "./RandomWalkerMouse";
-// import RandomWalkerWithParticle from "./RandomWalkerWithParticle";
-import SnakeGame from "./SnakeGame";
+import "./style.css";
 
-new P5(SnakeGame);
+function deleteAll() {
+  document.querySelectorAll("canvas").forEach((canvas: HTMLCanvasElement) => {
+    canvas.remove();
+  });
+}
+
+async function start() {
+  const selector = document.querySelector("#selector") as HTMLSelectElement;
+
+  const experiments = await import(`./**/index.ts`);
+
+  const experimentsNames = Object.keys(experiments).filter(
+    name => name !== "undefined" && name !== "default"
+  );
+
+  selector.value = experimentsNames[0];
+
+  experimentsNames.forEach(name => {
+    const option = document.createElement("option");
+    option.appendChild(document.createTextNode(name));
+    option.setAttribute("value", name);
+    selector.appendChild(option);
+  });
+
+  const select = async (name: string) => {
+    deleteAll();
+    new P5(experiments[name].default);
+  };
+
+  select(experimentsNames[0]);
+
+  selector.addEventListener("change", () => {
+    select(selector.value);
+  });
+}
+
+start();
